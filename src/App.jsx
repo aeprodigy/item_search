8179a27e0dc5e8ed04e323a7c1f327e28c4d2a98
aeprodigy <mikeapple056@gmail.com>
2024-01-13 11:01:43 +0200

    inputbox fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 
-import './App.css'
+import { useRef } from 'react'
+import './App.css';
+import { Form } from 'react-bootstrap';
 
 function App() {
-  
+ // useRef and Form.contol0
+ const inputdata = useRef(null); 
+
+ const handleInput=(event)=>{
+  event.preventDefault();
+ }
 
   return (
     <>
-     <h1 className='text-dark'>The Image Search</h1>
-     <input type='text'/>
+      <h1 className="text-dark">The Image Search</h1>
 
+      <form onSubmit={handleInput}>
+        
+        <Form.Control
+          type="search"
+          ref={inputdata}
+          placeholder="Please enter details"
+          className="SearchInput"
+        />
+      </form>
+    
     </>
-  )
+  );
 }
 
 export default App
